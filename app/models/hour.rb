da50bb5b36c422b5e995a192aeff320cc5c689f1
Louis Aude <louis.aude@live.fr>
2021-03-31 00:53:05 +0200

    minor style changes

--- app/models/hour.rb
+++ app/models/hour.rb
@@ -1,4 +1,9 @@
 class Hour < ApplicationRecord
   belongs_to :doctor
   belongs_to :health_care_company
+
+  WEEKDAY = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+  HOUR = (00..24)
+  MINUTE = (00..60)
+
 end
