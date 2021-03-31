272ba3c5c4068d4b7f586764657b411d0bca9dbf
Louis Aude <louis.aude@live.fr>
2021-03-31 21:35:43 +0200

    bugfix

--- app/models/hour.rb
+++ app/models/hour.rb
@@ -2,9 +2,6 @@ class Hour < ApplicationRecord
   belongs_to :doctor
   belongs_to :health_care_company
 
-  validates_numericality_of :closing_hour, 
-  greater_than: Proc.new { project.current_release }
-end
 
   WEEKDAY = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
   HOUR = (00..23)
