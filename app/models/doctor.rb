032537098bb40f3fa3eb8d40d1568f96f51a0281
Louis Aude <louis.aude@live.fr>
2021-04-02 12:32:44 +0200

    reverted bootstrap install as it came with undesirable stylsheets

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,13 @@
 class Doctor < ApplicationRecord
   belongs_to :health_care_company
   has_many :hours, dependent: :destroy
+  before_save :capitalize_attributes
+
+  private
+
+  def capitalize_attributes
+    attributes.select { |a| %w[name].include? a }.each do |attr, val|
+      send("#{attr}=", val.try(:strip).try(:capitalize))
+    end
+  end
 end
--- app/models/health_care_company.rb
+++ app/models/health_care_company.rb
@@ -1,4 +1,13 @@
 class HealthCareCompany < ApplicationRecord
   has_many :doctors, dependent: :destroy
   has_many :hours, dependent: :destroy
+  before_save :capitalize_attributes
+
+  private
+
+  def capitalize_attributes
+    attributes.select { |a| %w[name].include? a }.each do |attr, val|
+      send("#{attr}=", val.try(:strip).try(:capitalize))
+    end
+  end
 end
